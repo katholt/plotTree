ba6eace3f0e108f21c67c317b9cd1014530d4516
Kat Holt <kholt@unimelb.edu.au>
2015-04-15 18:58:18 +1000

    printing

--- shiny_practice/reactive/server.R
+++ shiny_practice/reactive/server.R
@@ -2,14 +2,14 @@ library(shiny)
 library(ape)
 source("plotTree.R")
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
 	tree <- eventReactive(input$drawButton, {
     	input$tree
   	})
 
 	info <- eventReactive(input$drawButton, {
-    	input$info
+    	input$info_file
   	})
 
 	heatmap <- eventReactive(input$drawButton, {
@@ -44,8 +44,64 @@ shinyServer(function(input, output) {
   	})
   	
   	
+  	highlight_column <- eventReactive(input$drawButton, {
+    	input$highlight_column
+  	})
+  	
+  	show_column <- eventReactive(input$drawButton, {
+    	input$show_column
+  	})
+  	
+  	
+  	# track data types
+  	chk_heatmap <- eventReactive(input$drawButton, {
+    	input$chk_heatmap
+  	})
+  	
+  	info_data <- eventReactive(input$drawButton, {
+    	input$info_data
+  	})
+  	
+  	returnDownload <- eventReactive(input$printButton, {
+    	input$returnDownload
+  	})
+  	
+  	download_type <- eventReactive(input$printButton, {
+    	input$download_type
+  	})
+  	
+  	h <- eventReactive(input$printButton, {
+    	input$h
+  	})
+
+  	w <- eventReactive(input$printButton, {
+    	input$w
+  	})
+  	  	
+	# An event observer for changes to INFO CSV file
+	observeEvent(input$info_file, 
+		{
+			# read the CSV file and get the column names.
+			# re-reading this file repeatedly is inefficient
+			df = read.table(input$info_file$datapath, header=TRUE, sep=',')
+			
+			# build a list of values, this is what is required by update methods
+			info_cols = list()
+			for (v in colnames(df)) {
+				info_cols[v] = v
+			}
+			
+			# update the two input widgets using the column names
+			updateSelectInput(session, inputId='highlight_column', choices=info_cols)
+			updateSelectInput(session, inputId='show_column', choices=info_cols)
+		}
+	)
+  	
+  	
   output$Tree <- renderPlot({
 
+	highlight_column <- highlight_column()
+	show_column <- show_column()
 	treeFile <- tree()
 	infoFile <- info()
     heatmapFile <- heatmap()
@@ -57,13 +113,36 @@ shinyServer(function(input, output) {
     end_col <- end_col()
     heatmap_breaks <- as.integer(heatmap_breaks())
     
+    chk_heatmap <- chk_heatmap()
+    info_data <- info_data()
+    
+    returnDownload <- returnDownload()
+    download_type <- download_type()
+    h <- h()
+    w <- w()
+    
     if (is.null(treeFile))
       return(NULL)
       
+    if (!info_data) {
+      infoFile <- NULL
+    } 
+    else {
+    	infoFile <- infoFile$datapath
+	}
+    
+    if (!chk_heatmap) {
+      heatmapFile <- NULL
+    } else {
+      heatmapFile <- heatmapFile$datapath
+    }
+
+      
     doPlotTree <-function(){  
     
-    	plotTree(tree=treeFile$datapath,infoFile=infoFile$datapath,
-      		heatmapData=heatmapFile$datapath,cluster=cluster,colourNodesBy=colour_nodes,
+    	plotTree(tree=treeFile$datapath,infoFile=infoFile,
+      		heatmapData=heatmapFile,cluster=cluster,colourNodesBy=highlight_column,
+      		infoCols=show_column,
       		tip.colour.cex=tip_size,heatmap.colours=colorRampPalette(c(start_col,middle_col,end_col),space="rgb")(heatmap_breaks)) 
     }
     
@@ -71,9 +150,9 @@ shinyServer(function(input, output) {
 
 ### SAVE FIGURE
       
-      if(input$returnDownload){
-if(input$type == 'PDF'){
-                pdf("plot.pdf", width=as.numeric(input$w*3.94), height=as.numeric(input$h*3.94))
+      if(returnDownload){
+if(download_type == 'PDF'){
+                pdf("plot.pdf", width=as.numeric(w*0.039370), height=as.numeric(h*0.039370))
                 doPlotTree()
                 dev.off()
 
@@ -82,8 +161,8 @@ if(input$type == 'PDF'){
             content <- function(file) {
                 file.copy("plot.pdf", file)
             })
-} else if (input$type == "PNG"){
-png("plot.png", width=as.numeric(input$w*3.94), height=as.numeric(input$h*3.94))
+} else if (download_type == "PNG"){
+png("plot.png", width=as.numeric(w), height=as.numeric(h), units='mm', res=200)
                 doPlotTree()
                 dev.off()
               output$pdflink <- downloadHandler(
@@ -92,7 +171,7 @@ png("plot.png", width=as.numeric(input$w*3.94), height=as.numeric(input$h*3.94))
                 file.copy("plot.png", file)
             })
 } else {
-stop(paste("Unexpected type returned:", input$type))
+stop(paste("Unexpected type returned:", download_type))
 }
             }
             
--- shiny_practice/reactive/ui.R
+++ shiny_practice/reactive/ui.R
@@ -1,29 +1,23 @@
 library(shiny)
 library(ape)
 library(RLumShiny)
-
 shinyUI(fluidPage(
   titlePanel("Plot tree"),
   sidebarLayout(
     sidebarPanel(
- 
       fileInput('tree', 'Choose tree file', multiple=F,
-                accept=c('biotree/newick', 
-								 '.nwk', '.tree')),
- 
-      fileInput('info', 'Choose info file', multiple=F,
-                accept=c('text/csv', 
-								 '.csv')),
-								 
-	textInput("colour_nodes", label = h4("Colour nodes by"), value = "Enter variable name"),
-
-	sliderInput("tip_size", label = h4("Tip size"), min = 0.1, 
-      		  max = 20, value = 0.5),
-						
-						
+                accept=c('biotree/newick','.nwk', '.tree')),
+      checkboxInput("info_data", "Info Data"),
+				conditionalPanel(
+					condition = "input.info_data",
+					fileInput('info_file', 'Info CSV'),
+					selectInput('show_column', 'Show Columns', c(''), multiple=TRUE),
+					selectInput('highlight_column', 'Highlight By', c('location')),
+					sliderInput("tip_size", label = h4("Tip size"), min = 0.1, 
+      		  max = 20, value = 0.5)
+				),			
 	### HEATMAP DATA
-	
-      checkboxInput("chk_heatmap", "Heatmap file", value=TRUE),
+      checkboxInput("chk_heatmap", "Heatmap file", value=FALSE),
       
       conditionalPanel(
         condition = "input.chk_heatmap", "Heatmap",
@@ -36,34 +30,32 @@ shinyUI(fluidPage(
 	  selectInput("clustering", label = "Columns clustering:", 
 				  choices = list("Select"=F, "Cluster based on density"=T, "Cluster according to tree"="square"), selected = "Select"), 
 				  "Note: You can only cluster according to tree if your rows are equal to your tree tips. I.e. if you're viewing the dataset against itself.",
-
 		 
 	 jscolorInput(inputId="start_col", label="Start colour", value="FFFFFF", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T),
      jscolorInput(inputId="middle_col", label="Middle colour", value="FFF94D", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T),
      jscolorInput(inputId="end_col", label="End colour", value="1755FF", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T),
 	 textInput("heatmap_breaks", label = "Breaks", value = "100")
-
-				  )
-
-	),
- 
- actionButton("drawButton", "Draw!"),
- 
- checkboxInput('returnDownload', 'Download figure?', FALSE),
- 
+				)
+		),
+ 	actionButton("drawButton", "Draw!")
+  ),
+ sidebarPanel(
+ 	checkboxInput('returnDownload', 'Download figure?', FALSE),
 	conditionalPanel(
             condition = "input.returnDownload == true",
-			radioButtons("type", "Download type:",
-             c(#"SVG" = "SVG",
-               "PDF" = "PDF",
-               "PNG" = "PNG")),            sliderInput(inputId="w", label = "width (A4=210mm):", min=60, max=600, value=210, width='80%', ticks=F),
+			radioButtons("download_type", "Download type:",
+             c("PDF" = "PDF",
+               "PNG" = "PNG")),            
+            sliderInput(inputId="w", label = "width (A4=210mm):", min=60, max=600, value=210, width='80%', ticks=F),
             sliderInput(inputId="h", label = "height (A4=297mm):", min=60, max=600, value=297, width='80%', ticks=F),
             br(),
             downloadLink('pdflink')    
-    )
+    ),
+    actionButton("printButton", "Update print settings")
     ),
     mainPanel(
-      plotOutput("Tree", height=800))
+      plotOutput("Tree", height=800)
     )
+)
 )
 )
