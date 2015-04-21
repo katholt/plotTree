e678ab418438c1afa68a359740488e28652c63fe
Kat Holt <kholt@unimelb.edu.au>
2015-04-21 12:50:00 +1000

    added functions up to snp file

--- plotTreeShiny/server.R
+++ plotTreeShiny/server.R
@@ -16,7 +16,7 @@ shinyServer( function(input, output, session) {
 				info_cols[v] = v
 			}
 			# update the two input widgets using the column names
-			updateSelectInput(session, inputId='colour_tips_by', choices=c('NA',info_cols))
+			updateSelectInput(session, inputId='colour_tips_by', choices=c('',info_cols))
 			updateSelectInput(session, inputId='print_column', choices=info_cols)
 		}
 	)
@@ -30,33 +30,77 @@ shinyServer( function(input, output, session) {
 		
 			treeFile <- input$tree$datapath
 			
+			# tree plotting options
+			label_tips <- input$label_tips
+			tree_line_width <- as.integer(input$tree_line_width)
+			branch_colour <- input$branch_colour
+			tipLabelSize <- as.integer(input$tipLabelSize)
+			offset <- as.integer(input$offset)
+			
 			# metadata variables
 			infoFile <- input$info_file$datapath
 			tip_size <- input$tip_size
 			colour_tips_by <- input$colour_tips_by
+			if (colour_tips_by == '') {colour_tips_by <- NA}
+			ancestral <- input$ancestral
+			pie_size <- input$pie_size
+			legend <- input$legend
+			legend_pos <- input$legend_pos
 			print_column <- input$print_column
 				
 			# heatmap variables
 			heatmap <- input$heatmap
 			cluster <- input$clustering
-			heat_start_col <- input$start_col
-			heat_middle_col <- input$middle_col
-			heat_end_col <- input$end_col
-			heatmap_breaks <- as.integer(input$heatmap_breaks)
-	
+			heatmapDecimalPlaces <- as.integer(input$heatmapDecimalPlaces)
+			colLabelCex <- as.integer(input$colLabelCex)
+			vlines_heatmap_col <-input$vlines_heatmap_col
+			vlines_heatmap <- input$vlines_heatmap
+			
+			heatColoursSpecify <- input$heatColoursSpecify
+			
+			if (heatColoursSpecify) {
+				heatmap_colours <- input$heatmap_colour_vector
+			}
+			else {
+				heatmap_colours <- colorRampPalette(c(input$start_col,input$middle_col,input$end_col),space="rgb")(as.integer(input$heatmap_breaks))
+			}
+			
+			# barplot variables
+			barData <- input$barData$datapath
+			barDataCol <- input$barDataCol
+			
+			# block plot variables
+			blockFile <- input$blockFile$datapath
+			block_colour <- input$block_colour
+			blwd <- input$blwd
+			genome_size <- input$genome_size
+			
+			snpFile <- input$snpFile
+			snp_colour <- input$snp_colour
 	
 			# TRACK DATA TYPES TO PLOT
 			chk_heatmap <- input$chk_heatmap
 			info_data <- input$info_data
+			chk_barplot <- input$chk_barplot
+			chk_blocks <- input$chk_blocks
+			chk_snps <- input$chk_snps
 	
-			if (is.null(treeFile))
-			  return(NULL)
+			if (is.null(treeFile)) { return(NULL) }
 	  
 			if (!info_data) { infoFile <- NULL } 
 			else { infoFile <- infoFile }
 	
 			if (!chk_heatmap) { heatmapFile <- NULL } 
-			else { heatmapFile <- heatmapFile$datapath }
+			else { heatmapFile <- heatmap$datapath }
+			
+			if (!chk_barplot) { barData <- NULL } 
+			else { barData <- barData }
+			
+			if (!chk_blocks) { blockFile <- NULL } 
+			else { blockFile <- blockFile }
+			
+			if (!chk_snps) { snpFile <- NULL } 
+			else { snpFile <- snpFile }
 
 		}) # end isolate
 
@@ -67,11 +111,21 @@ shinyServer( function(input, output, session) {
 		doPlotTree <-function() {  
 	
 			# underlying call to plotTree(), drawn to screen and to file
-			plotTree(tree=treeFile,
-				infoFile=infoFile, infoCols=print_column,
-				colourNodesBy=colour_tips_by, tip.colour.cex=tip_size,
+			plotTree(tree=treeFile, 
+				tip.labels=label_tips, tipLabelSize=tipLabelSize, offset=offset,
+				lwd=tree_line_width, edge.color=branch_colour,
+				infoFile=infoFile, infoCols=print_column, 
+				colourNodesBy=colour_tips_by, tip.colour.cex=tip_size, 
+				ancestral.reconstruction=ancestral, pie.cex=pie_size, 
+				legend=legend, legend.pos=legend_pos,
 				heatmapData=heatmapFile, cluster=cluster,
-				heatmap.colours=colorRampPalette(c(heat_start_col,heat_middle_col,heat_end_col),space="rgb")(heatmap_breaks)) 
+				heatmap.colours=heatmap_colours,
+				heatmapDecimalPlaces=heatmapDecimalPlaces, colLabelCex=colLabelCex,
+				vlines.heatmap=vlines_heatmap, vlines.heatmap.col=vlines_heatmap_col,
+				barData=barData, barDataCol=barDataCol,
+				blockFile=blockFile, block_colour=block_colour, blwd=blwd,
+				genome_size=genome_size,
+				snpFile=snpFile, snp_colour=snp_colour) 
 		}
 
 		doPlotTree()
--- plotTreeShiny/ui.R
+++ plotTreeShiny/ui.R
@@ -2,7 +2,7 @@ library(shiny)
 library(ape)
 library(RLumShiny)
 shinyUI(fluidPage(
-	titlePanel("Plot tree"),
+	#titlePanel("Plot tree"),
 	sidebarLayout(
 		sidebarPanel(
 		
@@ -10,8 +10,23 @@ shinyUI(fluidPage(
 			fileInput('tree', 'Upload tree file (nwk)', multiple=F,
 							accept=c('biotree/newick','.nwk', '.tree')),
 			
+			checkboxInput("treeOptions", h5("Change tree plotting options"), value=FALSE),
+			conditionalPanel(
+				condition = "input.treeOptions",
+				checkboxInput("label_tips", "Label tree tips?", value=FALSE),
+				
+				conditionalPanel(
+					condition = "input.label_tips",
+					textInput("tipLabelSize", label = "Text size", value = "1"),
+					textInput("offset", label = "Offset", value = "0")
+				),
+				
+				textInput("tree_line_width", label = "Branch width", value = "1.5"),
+				jscolorInput(inputId="branch_colour", label="Branch colour:", value="#000000", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T)
+			),	# finished tree options	
+				
 			### METADATA (info file)
-			checkboxInput("info_data", "Metadata"),
+			checkboxInput("info_data", h5("Add Metadata")),
 			
 			# OPTIONS TO DISPLAY IF METADATA CHECKED
 			conditionalPanel(
@@ -19,31 +34,86 @@ shinyUI(fluidPage(
 				fileInput('info_file', 'Upload metadata (CSV)'),
 				selectInput('print_column', 'Metadata columns to print:', c(''), multiple=TRUE),
 				selectInput('colour_tips_by', 'Colour tips by:', c('')),
-				sliderInput("tip_size", label = "Tip size", min = 0.1, max = 20, value = 0.5)
+				# options if colouring by tips
+				conditionalPanel(
+					condition = "input.colour_tips_by",
+					sliderInput("tip_size", label = "Tip size", min = 0.1, max = 20, value = 0.5),
+					### COLOUR PANELS
+					checkboxInput("legend", "Legend for node colours?", value=TRUE),
+					selectInput("legend_pos", label = "Position for legend", 
+						choices = list( "bottomleft"="bottomleft", "bottomright"="bottomright",
+										"top-left"="topleft", "topright"="topright")
+					), 
+					checkboxInput("ancestral", "Ancestral state reconstruction?", value=FALSE),
+					sliderInput("pie_size", label = "Pie graph size", min = 0.1, max = 20, value = 0.5)
+				)
 			),	# finished metadata options	
 
 			### HEATMAP DATA
-			checkboxInput("chk_heatmap", "Heatmap data", value=FALSE),
+			checkboxInput("chk_heatmap", h5("Add Heatmap Data"), value=FALSE),
 			
 			# OPTIONS TO DISPLAY IF HEATMAP CHECKED
 			conditionalPanel(
 				condition = "input.chk_heatmap",
-
 				fileInput('heatmap', 'Upload heatmap file (CSV)', multiple = F, accept = c('text/csv', '.csv')),
 				selectInput("clustering", label = h5("Clustering:"), 
 					choices = list("Select..."=F, "Cluster columns by values"=T, "Square matrix"="square"),
 					selected = "Select"), 
 				
 				# OPTIONALLY DISPLAY COLOUR OPTIONS
-				checkboxInput("heatColoursPrompt", "Change heatmap colours", value=FALSE),
+				checkboxInput("heatColoursPrompt", "Change heatmap colour ramp", value=FALSE),
 				conditionalPanel(
-					condition = "input.heatColoursPrompt", h5("Heatmap colour ramp:"),
+					condition = "input.heatColoursPrompt", h5("Heatmap colour ramp"),
 					jscolorInput(inputId="start_col", label="Start colour:", value="FFFFFF", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T),
 					jscolorInput(inputId="middle_col", label="Middle colour:", value="FFF94D", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T),
 					jscolorInput(inputId="end_col", label="End colour:", value="1755FF", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T),
 					textInput("heatmap_breaks", label = "Breaks:", value = "100")
-				)
+				),
+				checkboxInput("heatColoursSpecify", "Specify heatmap colours manually", value=FALSE),
+				conditionalPanel(
+					condition = "input.heatColoursSpecify", h5("Vector to use heatmap colours"),
+					textInput("heatmap_colour_vector", label = "R code", value = "rev(gray(seq(0,1,0.1)))")
+				),
+				textInput("heatmapDecimalPlaces", label = "Decimal places to show in heatmap legend:", value = "1"),
+				textInput("colLabelCex", label = "Text size for column labels:", value = "0.75"),
+				textInput("vlines_heatmap", label = "y-coordinates for vertical lines:", value = "c(2,5)"),
+				jscolorInput(inputId="vlines_heatmap_col", label="Colour for vertical lines:", value="1755FF", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T)				
+				
+				
 			), # finished heatmap options
+			
+			### BAR PLOT DATA
+			checkboxInput("chk_barplot", h5("Add Barplots"), value=FALSE),
+			
+			# OPTIONS TO DISPLAY IF HEATMAP CHECKED
+			conditionalPanel(
+				condition = "input.chk_barplot",
+				fileInput('barData', 'Upload data for bar plots (CSV)', multiple = F, accept = c('text/csv', '.csv')),
+				jscolorInput(inputId="barDataCol", label="Colour for barplots:", value="1755FF", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T)
+			), # finished bar plot options
+			
+			### GENOME BLOCKS DATA
+			checkboxInput("chk_blocks", h5("Plot genome blocks"), value=FALSE),
+			
+			# OPTIONS TO DISPLAY IF HEATMAP CHECKED
+			conditionalPanel(
+				condition = "input.chk_blocks",
+				fileInput('blockFile', 'Upload genome block coordinates', multiple = F, accept = c('text/tab', '.txt')),
+				textInput("genome_size", label = "Genome size (bp):", value = "5E6"),
+				jscolorInput(inputId="block_colour", label="Colour for blocks:", value="1755FF", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T),
+				sliderInput("blwd", label = "Block size", min = 0.1, max = 20, value = 5)
+			), # finished block plot options
+			
+			### SNP DATA
+			checkboxInput("chk_snps", h5("Plot SNP loci"), value=FALSE),
+			
+			# OPTIONS TO DISPLAY IF HEATMAP CHECKED
+			conditionalPanel(
+				condition = "input.chk_snps",
+				fileInput('snpFile', 'Upload SNP allele table (CSV)', multiple = F, accept = c('text/csv', '.csv')),
+				textInput("genome_size", label = "Genome size (bp):", value = "5E6"), # make this linked to previous conditional 
+				jscolorInput(inputId="snp_colour", label="Colour for SNPs:", value="1755FF", position = "bottom", color = "transparent", mode = "HSV", slider = T, close = T)
+			), # finished block plot options			
 
 			### DRAW BUTTON
 			actionButton("drawButton", "Draw!")
